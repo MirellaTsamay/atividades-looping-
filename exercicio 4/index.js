8bfdf1b01472b24e18c01412109d66dda9b9a8fd
MirellaTsamay <tsymimi@gmail.com>
2025-05-06 16:21:59 -0300

    Recuperação de conteúdo Flowgorithm (Prof Marcio)

--- exercicio 4/index.js
+++ exercicio 4/index.js
@@ -1,26 +1,26 @@
 let soma = 0;// Variável para guardar os números
 
-for (let i = 1; i <= 5; i++){
+for (let i = 1; i <= 5; i++) {
 
-const número = parseFloat(prompt(`Coloque a nota ${i}`))
-
-
-if (isNaN(número) || número < 0) { //verifica se o valor é valido 
+    const número = parseFloat(prompt(`Coloque a nota ${i}`))
 
 
-alert("Erro, coloque uma nota válido.");
+    if (isNaN(número) || número < 0) { //verifica se o valor é valido 
 
 
-} else {
+        alert("Erro, coloque uma nota válido."); // variavel que verificará se a nota nao for um numero
 
-    soma += número;
-    
-    
-    
 
-} 
+    } else {
+
+        soma += número;
+
+
+
+
+    }
 }
-media = soma /5; //calcula a media das notas
+media = soma / 5; //calcula a media das notas
 
 
 alert(`a média das notas que foram colocadas: ${media}\ o resultado das notas colocadas foram: ${soma}`);// Exibe a média
